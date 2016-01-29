a58dae0dc25863818d39aa257a95173ffa2c49c3
itsrainingdata <Zigmund-2@wifi-131-179-21-149.host.ucla.edu>
2016-01-29 10:59:35 -0800

    Now tracking graph library methods

---
+++ R/graph-compatibility.R
@@ -0,0 +1,87 @@
+#
+#  graph-compatibility.R
+#  sparsebnUtils
+#
+#  Created by Bryon Aragam (local) on 7/26/15.
+#  Copyright (c) 2014-2016 Bryon Aragam. All rights reserved.
+#
+
+#
+# Various utility functions for enforcing compatibility with the 'graph' package from BioConductor.
+#
+
+#
+# Helper function to convert an edge list to a graphNEL compatible edge list
+#  The main difference is instead of listing parents for each node, graphNEL requires
+#  listing children for each parent. There are also different naming and indexing conventions.
+#
+edge_list_to_graphNEL_edgeL <- function(el){
+    #----------- EXAMPLE -----------
+    # Default:
+    # [[1]]
+    # integer(0)
+    #
+    # [[2]]
+    # [1] 1
+    #
+    # [[3]]
+    # [1] 1
+    #
+    # graphNEL:
+    # $`1`
+    # $`1`$edges
+    # [1] 2 3
+    #
+    #
+    # $`2`
+    # $`2`$edges
+    # NULL
+    #
+    #
+    # $`3`
+    # $`3`$edges
+    # NULL
+    #
+    #-------------------------------
+
+    numnode <- length(el) # Number of nodes should be same as length of default edge list
+
+    ### Invert the child-parent relationships (aka implicit transpose of adjacency matrix)
+    el.graphNEL <- vector(mode = "list", length = numnode)
+    for(j in 1:numnode){
+        this.column <- el[[j]] # "column" = interpret as column in adj matrix
+        for(i in seq_along(this.column)){
+            el.graphNEL[[this.column[i]]] <- c(el.graphNEL[[this.column[i]]], j)
+        }
+    }
+
+    ### Needs an extra component called "edges" (allows for possible specification of weights as well)
+    el.graphNEL <- lapply(el.graphNEL, function(x){ list(edges = x)})
+
+    ### List names MUST be character 1,...,numnode
+    names(el.graphNEL) <- as.character(1:numnode)
+
+    el.graphNEL
+}
+
+to_graphNEL <- function(x) UseMethod("to_graphNEL", x)
+
+to_graphNEL.SparseBlockMatrixR <- function(sbm){
+    el <- edge.list(sbm)
+    el <- edge_list_to_graphNEL_edgeL(el)
+
+    graphNEL(nodes = as.character(1:num.nodes(sbm)), edgeL = el, edgemode = 'directed')
+}
+
+to_graphNEL.ccdrFit <- function(cf){
+    #
+    # REALLY SHOULD RENAME SBM SINCE THIS CAN CHANGE TYPE (SBM, matrix, graphNEL)
+    #
+    cf$sbm <- to_graphNEL(cf$sbm)
+
+    cf
+}
+
+to_graphNEL.ccdrPath <- function(cp){
+    lapply(cp, to_graphNEL)
+}
