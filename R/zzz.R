9c52c5df6d6319f710f5a245cf2ad19b1929e576
itsrainingdata <Zigmund-2@wifi-131-179-21-252.host.ucla.edu>
2016-01-28 17:18:06 -0800

    Export everything from this package by default

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,9 @@
 #  Copyright (c) 2016 Bryon Aragam. All rights reserved.
 #
 
+# Export everything in this package so the other packages can use all of the methods
+#' @exportPattern ^[^\\.]
+
 .onAttach <- function(libname, pkgname){
     packageStartupMessage("NOTE: This package is currently in a development state and may be unstable.\n Please report any bugs at https://github.com/itsrainingdata/sparsebnUtils/issues.")
 }
