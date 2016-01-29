b8a393000e1295eb4115cb1567357504ff0779c4
itsrainingdata <Zigmund-2@wifi-131-179-21-252.host.ucla.edu>
2016-01-28 17:52:47 -0800

    Now tracking files for sparsebnData

---
+++ R/s3-sparsebnData.R
@@ -0,0 +1,36 @@
+#
+#  s3-sparsebnData.R
+#  sparsebnUtils
+#
+#  Created by Bryon Aragam (local) on 1/28/16.
+#  Copyright (c) 2014-2016 Bryon Aragam. All rights reserved.
+#
+
+#------------------------------------------------------------------------------#
+# sparsebnData S3 Class for sparsebnUtils package
+#------------------------------------------------------------------------------#
+
+#
+# sparsebnData S3 class skeleton
+#
+# Data
+# *
+#
+# Methods
+# *
+#
+
+#' sparsebnData class
+#'
+#' Convenience wrapper class for observational and experimental data.
+#'
+#' Also inherits from \code{\link{list}}.
+#'
+#'
+#' @section Methods:
+#' \code{\link{first.method.name}}
+#' \code{\link{second.method.name}}
+#'
+#' @docType class
+#' @name sparsebnData-class
+NULL
