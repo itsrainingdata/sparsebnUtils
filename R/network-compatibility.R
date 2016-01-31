5e4bf8aea1a3e8fb49659a55e1a0879189c27813
itsrainingdata <Zigmund-2@MacBook-Pro-2.local>
2016-01-30 22:55:44 -0800

    Tracking network compatibility methods

---
+++ R/network-compatibility.R
@@ -0,0 +1,70 @@
+#
+#  network-compatibility.R
+#  sparsebnUtils
+#
+#  Created by Bryon Aragam (local) on 1/30/16.
+#  Copyright (c) 2014-2016 Bryon Aragam. All rights reserved.
+#
+
+#
+# Various utility functions for enforcing compatibility with the 'network' package (part of statnet).
+#
+
+to_network <- function(x) UseMethod("to_network", x)
+
+to_network.edgeList <- function(el){
+    ### This function requires the 'network' package to be installed
+    if (!requireNamespace("network", quietly = TRUE)) {
+        stop("network package required to coerce data to 'network' type!", call. = FALSE)
+    }
+
+    if(num.edges(el) > 0){
+        el.network <- edgeList_to_network_edgelist(el)
+
+        network::network(el.network, directed = TRUE)
+    } else{
+        #
+        # Is there a way to intialize a null graph using the network constructor?
+        #
+        network::network.initialize(num.nodes(el)) # hack to work around special case of null graph
+    }
+
+}
+
+to_network.sparsebnFit <- function(sbf){
+    sbf$edges <- to_network(sbf$edges)
+
+    sbf
+}
+
+to_network.sparsebnPath <- function(sbp){
+    lapply(sbp, to_network)
+}
+
+#
+# Helper function to convert an edge list to a network compatible edge list matrix
+#  The output is a 2 column matrix where each row represents one edge:
+#       [i j] = (i, j)
+#
+# NOTE: The required input is the _same_ as for igraph, so we just hijack that function.
+#
+edgeList_to_network_edgelist <- function(el){
+    #----------- EXAMPLE -----------
+    # Default:
+    # [[1]]
+    # integer(0)
+    #
+    # [[2]]
+    # [1] 1
+    #
+    # [[3]]
+    # [1] 1
+    #
+    # network:
+    # 1 2
+    # 1 3
+    #
+    #-------------------------------
+
+    edgeList_to_igraph_edgelist(el)
+}
