bb1bde706d8ba8ce77a3b45dd5f7f4fd1a39b4e2
itsrainingdata <bryonsgit@gmail.com>
2016-03-17 19:12:01 -0700

    Initial commit, header only

---
+++ R/sparsebnUtils-inference.R
@@ -0,0 +1,14 @@
+#
+#  sparsebnUtils-inference.R
+#  sparsebnUtils
+#
+#  Created by Bryon Aragam (local) on 3/17/16.
+#  Copyright (c) 2014-2016 Bryon Aragam. All rights reserved.
+#
+
+#
+# PACKAGE SPARSEBNUTILS: Methods for parameter inference
+#
+#   CONTENTS:
+#
+
