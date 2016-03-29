18a598e43ab8fcfe208540a9bf26d6070c07b980
itsrainingdata <bryonsgit@gmail.com>
2016-03-29 14:58:27 -0700

    Methods for returning various errors and warning messages

---
+++ R/sparsebnUtils-messages.R
@@ -0,0 +1,24 @@
+#
+#  sparsebnUtils-messages.R
+#  sparsebnUtils
+#
+#  Created by Bryon Aragam (local) on 3/29/16.
+#  Copyright (c) 2014-2016 Bryon Aragam. All rights reserved.
+#
+
+#
+# PACKAGE SPARSEBNUTILS: Messages
+#
+#   CONTENTS:
+#
+#
+
+input_not_sparsebnData <- function(data){
+    sprintf("Input data must be a valid sparsebnData object! <Current type: %s>", class(data))
+}
+
+alg_input_data_frame <- function(){
+    sprintf("Data input as a data.frame: In order to coerce your data to a valid sparsebnData object,
+             we will assume the data is purely observational. In the future, it's best to do this
+             yourself to prevent loss of information.")
+}
