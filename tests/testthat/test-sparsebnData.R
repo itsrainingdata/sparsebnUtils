05b94137554e7c7097227fd20593e0ae7e8c705c
itsrainingdata <bryonsgit@gmail.com>
2016-04-12 15:35:19 -0700

    Added partial matching for 'type'

--- tests/testthat/test-sparsebnData.R
+++ tests/testthat/test-sparsebnData.R
@@ -8,8 +8,16 @@ test_that("sparsebnData constructor fails if input not a list, data.frame, or ma
     expect_error(sparsebnData(rep(1,5)))
 })
 
-test_that("sparsebnData constructor fails if type not specified", {
+test_that("sparsebnData constructor fails if type not specified or improperly specified", {
     expect_error(sparsebnData(data = dat), regexp = "type")
+    expect_error(sparsebnData(data = dat, type = "contns"), regexp = "Invalid \'type\'")
+    expect_error(sparsebnData(data = dat, type = "dicsrt"), regexp = "Invalid \'type\'")
+
+    ### Check that partial matching works OK
+    expect_that(sparsebnData(data = dat, type = "c"), not(throws_error()))
+    expect_that(sparsebnData(data = dat, type = "cont"), not(throws_error()))
+    expect_that(sparsebnData(data = dat, type = "d"), not(throws_error()))
+    expect_that(sparsebnData(data = dat, type = "disc"), not(throws_error()))
 })
 
 test_that("print.sparsebnData functions properly", {
