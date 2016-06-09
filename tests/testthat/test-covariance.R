fd373c6e9e712248f4c62e66736ca3ff85752d5a
itsrainingdata <bryonsgit@gmail.com>
2016-06-08 17:43:24 -0700

    Updated old tests, added new tests for precision estimation

--- tests/testthat/test-covariance.R
+++ tests/testthat/test-covariance.R
@@ -4,7 +4,7 @@ dat_cts <- generate_continuous_sparsebnData()
 dat_disc <- generate_discrete_sparsebnData()
 
 test_that("Covariance estimation correctly recovers the identity", {
-    MAX_ERR <- 0.1 # a sufficiently large value that this should never fail
+    MAX_ERR <- 0.5 # a sufficiently large value that this should never fail
 
     ### Set up parameters
     pp <- 5
@@ -13,6 +13,7 @@ test_that("Covariance estimation correctly recovers the identity", {
     mvcov <- diag(rep(1, pp))
 
     ### Generate data
+    set.seed(1)
     dat <- matrix(rnorm(nn*pp), nrow = nn)
     dat <- sparsebnData(dat, type = "c")
 
@@ -24,4 +25,10 @@ test_that("Covariance estimation correctly recovers the identity", {
 test_that("Covariance estimation runs without errors", {
     expect_error(estimate.covariance(dat_cts), NA) # continuous data
     expect_error(estimate.covariance(dat_disc), "Covariance estimation for discrete models") # discrete data: not implemented yet
+})
+
+test_that("estimate.covariance returns expected output", {
+    out <- estimate.covariance(dat_cts)
+    expect_true(check_list_class(out, "Matrix"))
+    expect_true(class(out[[1]]) == "ddiMatrix") # first component should always be diagonal
 })
