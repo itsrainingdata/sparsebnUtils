054849f81713f3179b01d7a56ecbd691dab7f539
itsrainingdata <bryonsgit@gmail.com>
2016-07-14 12:26:25 -0700

    Changed test graph to have an isolated node

--- tests/testthat/test-get_solution.R
+++ tests/testthat/test-get_solution.R
@@ -4,9 +4,9 @@ sbp <- generate_nontrivial_sparsebnPath()
 
 test_that("Selection by number of edges works", {
     expect_equal(get.solution(sbp, edges = 0), sbp[[1]])
-    expect_equal(get.solution(sbp, edges = 5), sbp[[2]])
-    expect_equal(get.solution(sbp, edges = 6), sbp[[3]])
-    expect_equal(get.solution(sbp, edges = 8), sbp[[4]])
+    expect_equal(get.solution(sbp, edges = 4), sbp[[2]])
+    expect_equal(get.solution(sbp, edges = 5), sbp[[3]])
+    expect_equal(get.solution(sbp, edges = 7), sbp[[4]])
 })
 
 test_that("Selection by lambda works", {
