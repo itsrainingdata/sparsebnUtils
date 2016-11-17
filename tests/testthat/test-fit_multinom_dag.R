cb069041cb1df32722815dd5af89b3cd379944cf
gujyjean <gujy.lola@gmail.com>
2016-11-16 17:42:35 -0800

    update fit_multinom_dag and test files

--- tests/testthat/test-fit_multinom_dag.R
+++ tests/testthat/test-fit_multinom_dag.R
@@ -37,32 +37,22 @@ test_that("fit_multinom_dag can run", {
   expect_error(fit_multinom_dag(final.dag, data))
 })
 
-# test_that("fit_multinom_dag output the right result", {
-#   out <- fit_multinom_dag(edge_list, data) # input a data frame
-#   out_matrix <- fit_multinom_dag(edge_list, data_matrix) # input a matrix
-#
-#   ### length of output should be the number of variables
-#   expect_equal(length(out), node)
-#   expect_equal(length(out_matrix), node)
-#   ### legnth of each element should be the number of parents
-#   for (i in 1:node) {
-#     expect_equal(length(out[[i]]), length(edge_list[[i]])+(length(edge_list[[i]])!=0))
-#   }
-#   for (i in 1:node) {
-#       expect_equal(length(out_matrix[[i]]), length(edge_list[[i]])+(length(edge_list[[i]])!=0))
-#   }
-#   ### randomly check some entries
-#   expect_equal(dim(out[[1]][[1]]$coef), c(1, 1))
-#   expect_equal(out[[1]][[1]]$parent, "y")
-#   expect_equal(dim(out[[1]][[2]]$coef), c(1, 1))
-#   expect_equal(out[[1]][[2]]$parent, "a")
-#   expect_equal(dim(out[[3]][[1]]$coef), c(2, 1))
-#   expect_equal(out[[3]][[1]]$parent, "y")
-#   expect_equal(dim(out[[3]][[2]]$coef), c(2, 1))
-#   expect_equal(out[[3]][[2]]$parent, "b")
-#
-#   expect_equal(dim(out_matrix[[1]][[1]]$coef), c(1, 1))
-#   expect_equal(out_matrix[[1]][[1]]$parent, "V2")
-#   expect_equal(dim(out_matrix[[1]][[2]]$coef), c(1, 1))
-#   expect_equal(out_matrix[[1]][[2]]$parent, "V4")
-# })
+test_that("fit_multinom_dag output the right result", {
+  out <- fit_multinom_dag(edge_list, data) # input a data frame
+
+  n_levels <- c(2, 2, 3, 2, 2)
+  n_effect_levels <- n_levels-1
+
+  ### length of output should be the number of variables
+  expect_equal(length(out), node)
+
+  ### dimension of each element should be right
+  expect_equal(length(out[[1]]), 1+sum(n_effect_levels[edge_list[[1]]]))
+  expect_equal(length(out[[2]]), 1+sum(n_effect_levels[edge_list[[2]]]))
+  expect_equal(dim(out[[3]]), c(n_effect_levels[3], 1+sum(n_effect_levels[edge_list[[1]]])))
+
+  ### coefficient names are correct
+  expect_equal(names(out[[1]]), c("(Intercept)", "y_1", "a_1"))
+  expect_equal(names(out[[2]]), c("(Intercept)", "z_1", "z_2", "a_1", "b_1"))
+  expect_equal(colnames(out[[3]]), c("(Intercept)", "y_1", "b_1"))
+})
